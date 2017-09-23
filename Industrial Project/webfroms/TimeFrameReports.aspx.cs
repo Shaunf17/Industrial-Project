0cfebae173039e8da59dca899ec1395ba934e1aa
Krasimir Kostov <k_kostov95@abv.bg>
2017-09-23 20:38:06 +0100

    TimeFrameReports - Pie chart that changes according to the time frame.

---
+++ Industrial Project/webfroms/TimeFrameReports.aspx.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Text;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace Industrial_Project.webfroms
+{
+    public partial class TimeFrameReports : System.Web.UI.Page
+    {
+        protected static List<double> MyData = new List<double>();
+        protected static List<string> Location = new List<string>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public static List<double> getGraphData(string timeFrame)
+        {
+
+            SqlConnection con = new SqlConnection();
+            string connString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+            con.ConnectionString = connString;
+            SqlCommand cmd;
+            MyData.Clear();
+            if (timeFrame == "Last week")
+            {
+                cmd = new SqlCommand("Report_OneWeek", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    MyData.Add(double.Parse(rd[0].ToString()));
+
+                }
+                cmd.Dispose();
+                con.Close();
+                con.Dispose();
+
+                return MyData;
+            }
+            else if (timeFrame == "Last month")
+            {
+                cmd = new SqlCommand("Report_OneMonth", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    MyData.Add(double.Parse(rd[0].ToString()));
+
+                }
+                cmd.Dispose();
+                con.Close();
+                con.Dispose();
+
+                return MyData;
+            }
+            else if (timeFrame == "Last 3 months")
+            {
+                cmd = new SqlCommand("Report_ThreeMonths", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    MyData.Add(double.Parse(rd[0].ToString()));
+
+                }
+                cmd.Dispose();
+                con.Close();
+                con.Dispose();
+
+                return MyData;
+            }
+            else if (timeFrame == "Last 6 months")
+            {
+                cmd = new SqlCommand("Report_SixMonths", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    MyData.Add(double.Parse(rd[0].ToString()));
+
+                }
+                cmd.Dispose();
+                con.Close();
+                con.Dispose();
+
+                return MyData;
+            }
+            else if (timeFrame == "Last year")
+            {
+                cmd = new SqlCommand("Report_OneYear", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    MyData.Add(double.Parse(rd[0].ToString()));
+
+                }
+                cmd.Dispose();
+                con.Close();
+                con.Dispose();
+
+                return MyData;
+            }
+
+
+            return MyData;
+
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public static List<string> GetColumnData(string timeFrame)
+        {
+
+            SqlConnection con = new SqlConnection();
+            string connString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+            con.ConnectionString = connString;
+            SqlCommand cmd;
+            Location.Clear();
+            if (timeFrame == "Last week")
+            {
+                cmd = new SqlCommand("Report_OneWeek", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    Location.Add(rd[1].ToString());
+
+                }
+                cmd.Dispose();
+                con.Close();
+                con.Dispose();
+
+                return Location;
+            }
+            else if (timeFrame == "Last month")
+            {
+                cmd = new SqlCommand("Report_OneMonth", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    Location.Add(rd[1].ToString());
+
+                }
+                cmd.Dispose();
+                con.Close();
+                con.Dispose();
+
+                return Location;
+            }
+            else if (timeFrame == "Last 3 months")
+            {
+                cmd = new SqlCommand("Report_ThreeMonths", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    Location.Add(rd[1].ToString());
+
+                }
+                cmd.Dispose();
+                con.Close();
+                con.Dispose();
+
+                return Location;
+            }
+            else if (timeFrame == "Last 6 months")
+            {
+                cmd = new SqlCommand("Report_SixMonths", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    Location.Add(rd[1].ToString());
+
+                }
+                cmd.Dispose();
+                con.Close();
+                con.Dispose();
+
+                return Location;
+            }
+            else if (timeFrame == "Last year")
+            {
+                cmd = new SqlCommand("Report_OneYear", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                con.Open();
+
+                SqlDataReader rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    Location.Add(rd[1].ToString());
+
+                }
+                cmd.Dispose();
+                con.Close();
+                con.Dispose();
+
+                return Location;
+            }
+
+            return Location;
+
+        }
+    }
+}
