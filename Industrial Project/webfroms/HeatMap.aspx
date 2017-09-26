2779dcb18d6b2b8e74e5285804b2c52bc484bbbf
Shaun Falconer <shaunf1996@hotmail.co.uk>
2017-09-26 10:41:19 +0100

    Add files via upload
    Heat map that displays colour based on total usage throughout the week. Green = good, Red = bad

---
+++ Industrial Project/webfroms/HeatMap.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HeatMap.aspx.cs" Inherits="HeatmapTest.Webforms.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="/css/HeatMapStyle.css" />
+    <title>Heatmap</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Repeater ID="rptheatmap" runat="server">
+                <HeaderTemplate>
+                    <table id="heatmap">
+                </HeaderTemplate>
+
+                <ItemTemplate>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </ItemTemplate>
+
+                <FooterTemplate>
+                    </table>
+                </FooterTemplate>
+
+            </asp:Repeater>
+        </div>
+
+        <div>
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        </div>
+    </form>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script>
+        $(function () {
+            //$('tr > td:odd').each(function (index) {
+            $('table td:nth-child(1) ~ td').each(function () {
+                var scale = [['vPoor', 10], ['poor', 20], ['avg', 50], ['good', 150], ['vGood', 500]];
+                var score = $(this).text();
+                for (var i = 0; i < scale.length; i++) {
+                    if (score <= scale[i][1]) {
+                        $(this).addClass(scale[i][0]);
+                    }
+                }
+            });
+        });
+    </script>
+    <!--<script type="text/javascript">
+        //alert("hello");
+
+        var temp = document.getElementById("heatmap").getElementsByTagName("td");
+        var largest = 5;
+        for (var i = 0; i < temp.length; i++)
+        {
+            temp[i].innerHTML = i;
+        }
+        
+    </script>-->
+
+    <script>
+
+</script>
+</body>
+</html>
---
+++ Industrial Project/webfroms/HeatMap.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HeatmapTest.Webforms
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            populateTable();
+
+            //string conString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+            //Debug.Print("HELLO");
+            //Debug.Print(IsServerConnected(conString).ToString());
+        }
+
+        public void populateTable()
+        {
+            SqlConnection con = new SqlConnection();
+            string conString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+            con.ConnectionString = conString;
+            //SqlCommand cmd = new SqlCommand("SELECT [Outlet Name], SUM([Total Amount]) AS 'Total Amount' FROM YoyoDisbursals WHERE[Date & Time] >= '2015-10-10' AND[Date & Time] < '2015-10-11' GROUP BY[Outlet Name] ORDER BY[Outlet Name];", con);
+            //SqlCommand cmd = new SqlCommand("KrasimirTest @startDate = '2015-10-12'", con);
+            SqlCommand cmd = new SqlCommand("KrasimirTest", con);
+            SqlParameter startDate = new SqlParameter("@startDate", SqlDbType.DateTime);
+            startDate.Value = "2016-09-02";
+            cmd.Parameters.Add(startDate);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            con.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dataTable = new DataTable();
+
+            da.Fill(dataTable);
+
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+
+            con.Close();
+
+            //try
+            //{
+            //    //con.Open();
+            //    //SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            //    //DataSet ds = new DataSet();
+            //    //sda.Fill(ds);
+            //    //rptheatmap.DataSource = ds;
+            //    //rptheatmap.DataBind();
+
+            //    //con.Open();
+            //    //SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //    //DataTable dataTable = new DataTable();
+
+            //    //da.Fill(dataTable);
+
+            //    //GridView1.DataSource = dataTable;
+            //    //GridView1.DataBind();
+
+            //    //con.Close();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Debug.Print(ex.ToString());
+            //}
+            //finally
+            //{
+            //    //con.Close();
+            //}
+        }
+
+        private static bool IsServerConnected(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    return true;
+                }
+                catch (SqlException)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
